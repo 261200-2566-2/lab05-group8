8897c6540e727ea3a10a487edad4550a931ba677
Issawara <kmind46@gmail.com>
2023-12-04 18:57:50 +0700

    second commit

--- src/Lab05.java
+++ src/Lab05.java
@@ -3,39 +3,44 @@ public class Lab05 {
         Swordman m = new Swordman("Mind");
         Wizard f = new Wizard("Film");
         Assasin un = new Assasin("Unknow");
+
         SwordmanAcc rs = new SwordmanAcc("redSword","weapon","attack",50);
         SwordmanAcc rh = new SwordmanAcc("redHat","head","defend",50);
         AssasinAcc gh = new AssasinAcc("greenHat","head","defend",150);
+        //test receivedItem
         f.receivedItem("potion");
         f.receivedItem("potion");
         f.receivedItem("expCard");
-        m.statusWindow();
-        f.statusWindow();
-        m.statusWindow();
-        un.statusWindow();
-        un.equip(gh);
-        f.equip(rh);
-        f.statusWindow();
-        System.out.println("Mind exp = "+m.getExperience());
-        m.attack(f);
-        f.statusWindow();
-        System.out.println("Mind exp = "+m.getExperience());
-        m.attack(f);
-        f.statusWindow();
-        f.useitem();
-        f.useitem();
-        System.out.println("Mind exp = "+m.getExperience());
-        m.attack(f);
-        System.out.println("Mind exp = "+m.getExperience());
+        //status after create character base on their job
         m.statusWindow();
         f.statusWindow();
         un.statusWindow();
-        m.attack(un);
+        //test equipment
+        un.equip(gh);//wearable
+        f.equip(rh);//not wearable (not pass requirement)
+
+        System.out.println("Mind exp = "+m.getExperience()); //exp before attack
+        m.attack(f); //attack
+        f.statusWindow(); //status after be attacked
+        System.out.println("Mind exp = "+m.getExperience()); //exp after attack
+        m.attack(f); //attack
+        f.statusWindow(); //status after be attacked
+        f.useitem(); //use item
+        f.statusWindow();//hp after use potion or exp after use expCard
+        System.out.println("Mind exp = "+m.getExperience());
+        m.attack(f);
+        System.out.println("Mind exp = "+m.getExperience());
+        m.statusWindow(); //after Lv up
+        un.statusWindow(); //show that unknow's def more than mind's dam
+        m.attack(un);//show defend
+        //show accessory description
         rs.getDescription();
         rh.getDescription();
         gh.getDescription();
+        //equip many accessory
         m.equip(rs);
         m.equip(rh);
+        m.equip(rh);//head is full. can't equip.
         m.statusWindow();
     }
 }
